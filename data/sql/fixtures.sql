19eecf9740a4dcb96a0feb2d5c1205bbfc9f78dc
Stefano Valle <s.valle@mvassociati.it>
2014-02-01 12:00:52 +0100

    Update data fixtures

--- data/sql/fixtures.sql
+++ data/sql/fixtures.sql
@@ -6,19 +6,9 @@ INSERT INTO country VALUES (1, 'it', 'Italy', 'italy');
 INSERT INTO country VALUES (2, 'gb', 'United Kingdom', 'united-kingdom');
 
 --
--- Name: country_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
---
-
-SELECT pg_catalog.setval('country_id_seq', 1, false);
-
---
 -- Data for Name: event; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-INSERT INTO event VALUES (1, 1, 'PhpDay 2013', 'The italian PHP conference', '2013-05-17 00:00:00', '2013-05-18 00:00:00', 'Verona', 'San Marco Hotel', 180, 'http://2013.phpday.it/');
-INSERT INTO event VALUES (2, 1, 'JsDay 2013', 'The most used language, the most needed conference', '2013-05-15 00:00:00', '2013-05-16 00:00:00', 'Verona', 'Hotel San Marco', 180, 'http://2013.jsday.it/');
-
---
--- Name: event_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
---
-SELECT pg_catalog.setval('event_id_seq', 5, true);
+INSERT INTO event VALUES (1, 1, 'Zend Framework Day 2014', 'La conferenza italiana su Zend Framework 2', '2014-02-07 00:00:00', '2014-02-07 00:00:00', 'Torino', 'I3P', 20, 'http://2014.zfday.it/');
+INSERT INTO event VALUES (2, 1, 'PhpDay 2014', 'The italian PHP conference', '2014-05-16 00:00:00', '2014-05-17 00:00:00', 'Verona', 'San Marco Hotel', 180, 'http://2014.phpday.it/');
+INSERT INTO event VALUES (3, 1, 'JsDay 2014', 'The most used language, the most needed conference', '2014-05-14 00:00:00', '2014-05-15 00:00:00', 'Verona', 'Hotel San Marco', 180, 'http://2014.jsday.it/');
