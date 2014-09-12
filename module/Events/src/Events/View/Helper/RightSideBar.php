429d95d4c3bce7541952118eb5f6f792dae534dd
Stefano Valle <s.valle@mvassociati.it>
2014-09-12 13:19:33 +0200

    REFACTOR

--- module/Events/src/Events/View/Helper/RightSideBar.php
+++ module/Events/src/Events/View/Helper/RightSideBar.php
@@ -15,30 +15,10 @@ class RightSideBar extends AbstractHelper {
         
     }
     
-	public function __invoke() {
-            
-		
-		$html = '<aside id="sidebar" class="fr">
-		<div class="rounded-box-title box">
-		<div class="box-inner clearfix">
-		<form id="country-form" action="/events" method="get" class="sidebar-form">
-		<div class="layout-slider">
-		</div>
-		<p><label for="location-1">Location: </label>
-		<select name="country" id="country">';
-		foreach ($this->countries as $name) {
-			$html .= '<option>' . $name . '</option>';
-		}
-		$html .= '</select></p>
-		<p><a href="#" onclick="document.getElementById(\'country-form\').submit();" class="bigbutton">Refine</a></p>
-		</form>
-		</div>
-		</div><!-- END: box -->
-		</aside>
-        <!-- END: sidebar -->';
-	    
-	    return $html;
-	    
-	}
+    public function __invoke() {
+
+        return $this->view->render('partials/filterbox.phtml', array('countries' => $this->countries));
+
+    }
 
 }
