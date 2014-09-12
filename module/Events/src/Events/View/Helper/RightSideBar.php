e94bb8ebdba336c8ff79df50a62617cfc79a7e30
Stefano Valle <s.valle@mvassociati.it>
2014-09-12 13:07:19 +0200

    GREEN

--- module/Events/src/Events/View/Helper/RightSideBar.php
+++ module/Events/src/Events/View/Helper/RightSideBar.php
@@ -7,16 +7,31 @@ use Zend\Http\Request;
 
 class RightSideBar extends AbstractHelper {
 
+    private $countries;
+    
     public function __construct() {
-                
+            
+        $this->countries = array('Italy', 'UK');
+        
     }
     
 	public function __invoke() {
+            
 		
 		$html = '<aside id="sidebar" class="fr">
 		<div class="rounded-box-title box">
 		<div class="box-inner clearfix">
-		
+		<form id="country-form" action="/events" method="get" class="sidebar-form">
+		<div class="layout-slider">
+		</div>
+		<p><label for="location-1">Location: </label>
+		<select name="country" id="country">';
+		foreach ($this->countries as $name) {
+			$html .= '<option>' . $name . '</option>';
+		}
+		$html .= '</select></p>
+		<p><a href="#" onclick="document.getElementById(\'country-form\').submit();" class="bigbutton">Refine</a></p>
+		</form>
 		</div>
 		</div><!-- END: box -->
 		</aside>
