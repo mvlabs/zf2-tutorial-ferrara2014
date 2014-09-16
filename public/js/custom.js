66265112cbc58f9b3f76d199789a4be938219441
ZF2 <zf2@zf2-VirtualBox.(none)>
2014-09-17 00:35:14 +0200

    inlineScript(), headStyle() helpers usage

--- public/js/custom.js
+++ public/js/custom.js
@@ -2,18 +2,12 @@
 
 jQuery(function(){
 
-
-	/*Rating*/
-	$('.popularity').raty({
-	  half:  true,
-	  start: 0
-	});
-	$('.popularity-big').raty({
-		half:true,
-		size:24,
-		starOff:'big-hub-off.png',
-		starOn:	'big-hub-on.png',
-		starHalf:'big-hub-half.png'
-	});
-	
+        /*
+        $( document ).ready(function() {
+            alert(x);
+        });
+        */
+    
 });
+
+
